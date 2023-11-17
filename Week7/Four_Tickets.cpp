08e34cf0e153e656a84b509c071a63f804e393d4
haqueWasif <123668473+haqueWasif@users.noreply.github.com>
2023-11-18 00:58:15 +0600

    Add files via upload

---
+++ Week7/Four_Tickets.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#define fastIO() ios_base :: sync_with_stdio(false); cin.tie(NULL);
+#define forI(i, s, n) for(i=s; i<n; i++)
+#define forI_R(i, n, s) for(i=n; i>=s; i--)
+#define inputArr(n, arr) for(int i=0; i<n; i++){cin >> arr[i];}
+#define forI_list(it, l) for(auto it=l.begin(); it != l.end(); it++)
+#define ll long long 
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vector<int>>
+#define vvll vector<vector<long long>>
+#define u_mii unordered_map<int,int>
+#define mii map<int,int>
+#define mic map<int,char>
+#define msi map<string,int>
+#define pii pair<int,int>
+#define pq_max priority_queue<int>
+#define pq_min priority_queue<int, vector<int>, greater<int>>
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define nl '\n'
+using namespace std;
+
+void solve(){
+    int x; cin >> x;
+
+    if(4*x <= 1000) yes;
+    else no;
+    
+}
+
+int main(){
+    fastIO();
+
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
